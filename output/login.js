a95f5b7d174c5c1359bb8f6d671d294d7651b247
Fatem <f.s4remi@gmail.com>
2020-03-15 22:51:36 -0700

    resolved relative url issue

--- output/login.js
+++
@@ -1,8 +0,0 @@
-const getUserInfo = () => {
-    const button = document.getElementById('log in button');
-	button.addEventListener('click', () => {
-		console.log('Email: '+ useremail.value + ' password:' + userpassword.value);
-    });
-};
-
-window.onload = getUserInfo;
--- output/navClick.js
+++
@@ -1,7 +0,0 @@
-const navOverlayHandler = () => {
-    document.getElementById('overlay').addEventListener('click', ()=> {
-        document.getElementById('navigation').style = 'display : block';
-    });
-};
-
-window.onload = navOverlayHandler;
